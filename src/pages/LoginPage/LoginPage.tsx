d251a1e293b20c037a833b18fbb91b38c6d42d6c
Sergio <sergiot.fontal@gmail.com>
2023-04-06 22:32:16 +0200

    Refactor LoginPage component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,22 +1,29 @@
+import { Link } from "react-router-dom";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import LoginPageStyled from "./LoginPageStyled";
 
 const LoginPage = (): JSX.Element => {
   return (
-    <LoginPageStyled>
-      <div className="heading">
+    <LoginPageStyled className="login">
+      <header className="login__header header">
         <span>Hello!</span>
-        <h2>
-          Sign into your <span className="highlighted">account</span>
+        <h2 className="header__title title">
+          Sign into your <span className="title__highlighted">account</span>
         </h2>
-      </div>
-      <LoginForm></LoginForm>
-      <span>
-        Don't have an account?{" "}
-        <a href="/login" className="highlighted">
-          Register Now
-        </a>
-      </span>
+      </header>
+      <LoginForm />
+      <footer className="login__footer footer">
+        <span className="footer__register-option register-option">
+          Don't have an account?{" "}
+          <Link
+            to="/login"
+            className="register-option__highlighted"
+            aria-label="link to user register form"
+          >
+            Register Now!
+          </Link>
+        </span>
+      </footer>
     </LoginPageStyled>
   );
 };
