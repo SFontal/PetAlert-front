b99400e9c0d63dabd1bfde8fa36d0ed645aa4855
Sergio <sergiot.fontal@gmail.com>
2023-04-06 20:17:41 +0200

    Change route /users to /user and rename pets mocks file

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,15 +1,16 @@
 import { rest } from "msw";
-import petsTest from "./petsTest";
+import pets from "./pets";
+import { mockedToken } from "./token";
 
 const apiUrl = process.env.REACT_APP_URL!;
-const routes = { user: "/users", login: "/login", pets: "/pets" };
+const routes = { user: "/user", login: "/login", pets: "/pets" };
 
 const handlers = [
   rest.post(`${apiUrl}${routes.user}${routes.login}`, async (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ token: "mockedToken" }));
+    return res(ctx.status(200), ctx.json({ token: mockedToken }));
   }),
   rest.get(`${apiUrl}${routes.pets}`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(petsTest));
+    return res(ctx.status(200), ctx.json(pets));
   }),
 ];
 
