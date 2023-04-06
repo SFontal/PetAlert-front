44f9950fea2aa5ed5131f8e736db2787b50d3bd8
Sergio <sergiot.fontal@gmail.com>
2023-04-06 23:31:30 +0200

    Refactor CardList component

--- src/components/CardList/CardList.test.tsx
+++ src/components/CardList/CardList.test.tsx
@@ -1,16 +1,28 @@
 import { screen } from "@testing-library/react";
-import petsTest from "../../mocks/petsTest";
+import pets from "../../mocks/pets";
 import { renderRouterWithProviders } from "../../testUtils/renderRouterWithProviders";
 import CardList from "./CardList";
 
 describe("Given a CardList component", () => {
-  describe("When it's rendered", () => {
+  describe("When it is rendered", () => {
     test("Then it should show a list of pets", () => {
-      renderRouterWithProviders(<CardList />, { pets: petsTest });
+      renderRouterWithProviders(<CardList />, { pets });
 
       const expectedPetList = screen.getByRole("list");
 
       expect(expectedPetList).toBeInTheDocument();
+    });
+  });
+
+  describe("When there is a list of three pets and it is rendered", () => {
+    test("Then it should show a list of three pets", () => {
+      const listOfThreePets = pets;
+
+      renderRouterWithProviders(<CardList />, { pets: listOfThreePets });
+
+      const expectedPetList = screen.getAllByRole("listitem");
+
+      expect(expectedPetList).toHaveLength(3);
     });
   });
 });
