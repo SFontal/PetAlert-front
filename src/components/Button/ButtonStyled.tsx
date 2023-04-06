8f94e16ae6118b8cd1dc17b56e174c893dbaf280
Sergio <sergiot.fontal@gmail.com>
2023-04-06 20:30:29 +0200

    Refactor Button component

--- src/components/Button/ButtonStyled.tsx
+++ src/components/Button/ButtonStyled.tsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 
 const ButtonStyled = styled.button`
+  align-items: center;
   background-color: ${(props) => props.theme.colors.terciary};
   border: solid 2px ${(props) => props.theme.colors.terciary};
   border-radius: 5px;
   color: ${(props) => props.theme.colors.main};
+  display: flex;
   font-family: "Baloo Bhai 2";
   font-size: 1.25rem;
-  height: 40px;
-  align-self: center;
+  height: 30px;
+  justify-content: center;
+  padding: 5px;
 
   :disabled {
     opacity: 0.75;
