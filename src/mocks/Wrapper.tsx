df1beed52234a6152031ac00ccade2bc73113a71
Sergio <sergiot.fontal@gmail.com>
2023-04-06 19:41:57 +0200

    Wrap the Wrapper in BrowserRouter

--- src/mocks/Wrapper.tsx
+++ src/mocks/Wrapper.tsx
@@ -1,9 +1,14 @@
 import { PropsWithChildren } from "react";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "../store";
 
 const Wrapper = ({ children }: PropsWithChildren): JSX.Element => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <BrowserRouter>
+      <Provider store={store}>{children}</Provider>
+    </BrowserRouter>
+  );
 };
 
 export default Wrapper;
