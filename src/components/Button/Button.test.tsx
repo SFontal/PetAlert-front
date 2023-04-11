d32b3bd19b7c9f2419e49efcadf9b9ff82451a4b
Sergio <sergiot.fontal@gmail.com>
2023-04-11 21:39:05 +0200

    Test new features and improve current Button tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,34 +1,83 @@
+import { mockedUiState } from "../../mocks/ui";
+import { renderRouterWithProviders } from "../../testUtils/renderRouterWithProviders";
 import { renderWithProviders } from "../../testUtils/renderWithProviders";
 import Button from "./Button";
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-describe("Given a Button componet", () => {
-  describe("When it receives 'View +' text and it is rendered", () => {
-    test("The it should show a button with 'View +' on it", () => {
-      const text = "View +";
-      const className = "view-more";
+describe("Given a Button component", () => {
+  const text = "Register!";
+  const className = "register";
+  const buttonLabel = "click to register";
 
-      renderWithProviders(<Button className={className} text={text} />);
+  describe("When it receives 'Register!' text, 'register' class, 'click to register' aria-label and it is rendered", () => {
+    test("Then it should show a button with 'Register!' on it", () => {
+      renderWithProviders(
+        <Button className={className} text={text} ariaLabel={buttonLabel} />
+      );
 
-      const expectedButton = screen.getByRole("button", { name: text });
+      const expectedButton = screen.getByRole("button", { name: buttonLabel });
 
       expect(expectedButton).toBeInTheDocument();
+    });
+
+    test("Then it should show the same button as before but disabled if it receives it setted as true", () => {
+      const disabled = true;
+
+      renderWithProviders(
+        <Button className={className} text={text} ariaLabel={buttonLabel} />,
+        { ui: { ...mockedUiState, isSubmitDisabled: disabled } }
+      );
+
+      const expectedButton = screen.getByRole("button", { name: buttonLabel });
+
+      expect(expectedButton).toBeDisabled();
+    });
+  });
+
+  describe("When it receives, in addition to the above, an onClick function and the user click on the button", () => {
+    test("Then it should call the function", async () => {
+      const onClickFunction = jest.fn();
+      const disabled = false;
+
+      renderWithProviders(
+        <Button
+          className={className}
+          text={text}
+          ariaLabel={buttonLabel}
+          onClick={onClickFunction}
+        />,
+        { ui: { ...mockedUiState, isSubmitDisabled: disabled } }
+      );
+
+      const button = screen.getByRole("button", { name: buttonLabel });
+
+      await userEvent.click(button);
+
+      expect(onClickFunction).toHaveBeenCalled();
     });
   });
 
   describe("When it receives 'Login!' text, a true isLink prop, and it is rendered", () => {
-    test("The it should show a link with 'Login!' on it", () => {
+    test("Then it should show a link with 'Login!' on it", () => {
       const text = "Login!";
       const className = "login";
       const path = "/login";
+      const linkLabel = "link to login page";
 
-      renderWithProviders(
-        <Button className={className} text={text} path={path} isLink={true} />
+      renderRouterWithProviders(
+        <Button
+          className={className}
+          text={text}
+          path={path}
+          ariaLabel={linkLabel}
+          isLink={true}
+        />
       );
 
-      const expectedButton = screen.getByRole("link");
+      const expectedLink = screen.getByRole("link", { name: linkLabel });
 
-      expect(expectedButton).toBeInTheDocument();
+      expect(expectedLink).toBeInTheDocument();
     });
   });
 });
