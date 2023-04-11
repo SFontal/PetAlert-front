12452cc0eddcbcbc988a48cc53cc83fd90b4d302
Sergio <sergiot.fontal@gmail.com>
2023-04-12 01:35:22 +0200

    Add Greeting component to Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { useAppSelector } from "../../store/hooks";
 import Header from "../Header/Header";
 import Modal from "../Modal/Modal";
+import Greeting from "../Greeting/Greeting";
 
 const Layout = (): JSX.Element => {
+  const { pathname } = useLocation();
   const { isOpen } = useAppSelector((state) => state.ui);
+
   return (
     <>
       <Header />
+      {!["/login", "/register"].includes(pathname) && <Greeting />}
       <main aria-label="Find your new family member">
         {isOpen && <Modal />}
         <Outlet />
