a6087b65232aa02deb3245bd0773f591237ea592
Sergio <sergiot.fontal@gmail.com>
2023-04-11 17:52:56 +0200

    Test ableSubmit and disableSubmit functions and refactor current tests

--- src/store/features/uiSlice/uiSlice.test.ts
+++ src/store/features/uiSlice/uiSlice.test.ts
@@ -1,32 +1,88 @@
-import { ModalState } from "../types";
+import { ModalState, UiState } from "../types";
+import { mockedUiState } from "../../../mocks/ui";
 import {
+  ableSubmitActionCreator,
   closeModalActionCreator,
+  disableSubmitActionCreator,
   openModalActionCreator,
   uiReducer,
 } from "./uiSlice";
 
-describe("Given an openModal reducer", () => {
-  describe("When it receives a modal state and an openModal action", () => {
-    test("Then it should return the same modal with his property isOpen set as true", () => {
-      const modal: ModalState = { isOpen: false, isError: false };
+describe("Given the ui reducer", () => {
+  describe("When it receives a modal state closed and an open modal action", () => {
+    test("Then it should return ui state with his property isOpen setted as true", () => {
+      const modalStateClosed: ModalState = { isOpen: false, isError: false };
+      const newUiState: UiState = {
+        ...mockedUiState,
+        isOpen: true,
+      };
 
-      const expectedModal: ModalState = { isOpen: true, isError: false };
+      const expectedUiState: UiState = uiReducer(
+        { ...mockedUiState },
+        openModalActionCreator(modalStateClosed)
+      );
 
-      const modalOpened = uiReducer(modal, openModalActionCreator(modal));
-
-      expect(modalOpened).toStrictEqual(expectedModal);
+      expect(newUiState).toStrictEqual(expectedUiState);
     });
   });
 
-  describe("When it receives a modal state and a closeModal action", () => {
-    test("Then it should return the same modal with his properties isOpen and isError set as false", () => {
-      const modal: ModalState = { isOpen: true, isError: true };
+  describe("When it receives a close modal action", () => {
+    test("Then it should return the ui state with his property isOpen setted as false", () => {
+      const currentUiState: UiState = {
+        ...mockedUiState,
+        isOpen: true,
+      };
+      const newUiState: UiState = {
+        ...mockedUiState,
+        isOpen: false,
+      };
 
-      const expectedModal: ModalState = { isOpen: false, isError: false };
+      const expectedUiState: UiState = uiReducer(
+        { ...currentUiState },
+        closeModalActionCreator()
+      );
 
-      const modalClosed = uiReducer(modal, closeModalActionCreator());
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
 
-      expect(modalClosed).toStrictEqual(expectedModal);
+  describe("When it receives an able submit action", () => {
+    test("Then it should return the ui state with his property isSubmitDisabled setted as false", () => {
+      const currentUiState: UiState = {
+        ...mockedUiState,
+        isSubmitDisabled: true,
+      };
+      const newUiState: UiState = {
+        ...mockedUiState,
+        isSubmitDisabled: false,
+      };
+
+      const expectedUiState: UiState = uiReducer(
+        { ...currentUiState },
+        ableSubmitActionCreator()
+      );
+
+      expect(newUiState).toStrictEqual(expectedUiState);
+    });
+  });
+
+  describe("When it receives a disable submit action", () => {
+    test("Then it should return the ui state with his property isSubmitDisabled setted as true", () => {
+      const currentUiState: UiState = {
+        ...mockedUiState,
+        isSubmitDisabled: false,
+      };
+      const newUiState: UiState = {
+        ...mockedUiState,
+        isSubmitDisabled: true,
+      };
+
+      const expectedUiState: UiState = uiReducer(
+        { ...currentUiState },
+        disableSubmitActionCreator()
+      );
+
+      expect(newUiState).toStrictEqual(expectedUiState);
     });
   });
 });
