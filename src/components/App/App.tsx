41e379c8bb2bfde9688642b2dbb3e410ed3b22de
Sergio <sergiot.fontal@gmail.com>
2023-04-12 12:09:07 +0200

    Add getToken function to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,14 @@
+import useToken from "../../hooks/useToken/useToken";
+import { useEffect } from "react";
 import Layout from "../Layout/Layout";
 
 const App = (): JSX.Element => {
+  const { getToken } = useToken();
+
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
+
   return <Layout />;
 };
 
