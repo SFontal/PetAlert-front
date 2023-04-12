870749dc3abea2b6789d4d332de9d54f06049187
Sergio <sergiot.fontal@gmail.com>
2023-04-12 11:05:22 +0200

    Test link and subtitle elements in Header component

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -3,23 +3,49 @@ import { renderWithProviders } from "../../testUtils/renderWithProviders";
 import Header from "./Header";
 
 describe("Given a Header component", () => {
-  describe("When its rendered", () => {
-    test("Then it should show an image as the website logo", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show link with the website logo and name", () => {
+      const linkLable = "link to pet alert home page";
+
       renderWithProviders(<Header />);
+
+      const expectedLink = screen.getByRole("link", { name: linkLable });
+
+      expect(expectedLink).toBeInTheDocument();
+    });
+
+    test("Then it should show the website logo", () => {
       const imageAlt = "pet alert logo";
+
+      renderWithProviders(<Header />);
 
       const expectedLogo = screen.getByRole("img", { name: imageAlt });
 
       expect(expectedLogo).toBeInTheDocument();
     });
 
-    test("Then it should show a heading level 1 with the text 'pet alert!'", () => {
-      renderWithProviders(<Header />);
+    test("Then it should show the website name 'pet alert!'", () => {
       const text = "pet alert!";
+      const level = 1;
 
-      const expectedHeading = screen.getByRole("heading", { name: text });
+      renderWithProviders(<Header />);
+
+      const expectedHeading = screen.getByRole("heading", {
+        name: text,
+        level,
+      });
 
       expect(expectedHeading).toBeInTheDocument();
+    });
+
+    test("Then it should show the website subtitle 'adopt, rescue, love!'", () => {
+      const subtitle = "adopt, rescue, love!";
+
+      renderWithProviders(<Header />);
+
+      const expectedSubtitle = screen.getByText(subtitle);
+
+      expect(expectedSubtitle).toBeInTheDocument();
     });
   });
 });
