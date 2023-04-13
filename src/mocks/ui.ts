81d3cf1ffb3bb32aedc45ca93b19f73e8ed85864
Sergio <sergiot.fontal@gmail.com>
2023-04-13 16:31:15 +0200

    Update mockedUiState and useUser

--- src/mocks/ui.ts
+++ src/mocks/ui.ts
@@ -3,5 +3,6 @@ import { UiState } from "../store/features/types";
 export const mockedUiState: UiState = {
   isError: false,
   isOpen: false,
+  isPasswordHide: true,
   isSubmitDisabled: true,
 };
