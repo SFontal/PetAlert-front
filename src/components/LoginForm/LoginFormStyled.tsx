e62a08f39e26c97c5d22cdd160b72e4852ec97f5
Sergio <sergiot.fontal@gmail.com>
2023-04-14 14:13:45 +0200

    Extract inputs and icons style

--- src/components/LoginForm/LoginFormStyled.tsx
+++ src/components/LoginForm/LoginFormStyled.tsx
@@ -8,56 +8,6 @@ const LoginFormStyled = styled.form`
   gap: 20px;
   padding: 40px 10px;
 
-  .email,
-  .password {
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-
-    &-box {
-      align-items: center;
-      display: flex;
-
-      &__input {
-        border-radius: 5px;
-        height: 40px;
-        padding: 10px;
-        width: 100%;
-
-        :disabled {
-          outline-offset: -1px;
-          outline: solid 2px white;
-        }
-
-        :focus {
-          outline-color: ${(props) => props.theme.colors.quaternary};
-        }
-
-        &::placeholder {
-          color: ${(props) => props.theme.colors.placeholder};
-        }
-      }
-
-      &__icon {
-        align-items: center;
-        background-color: ${(props) => props.theme.colors.terciary};
-        border-radius: 0 4px 4px 0;
-        display: flex;
-        height: 38px;
-        justify-content: center;
-        padding: 0 5px;
-        position: absolute;
-        right: 21px;
-        width: 40px;
-
-        svg {
-          height: 30px;
-          width: 30px;
-        }
-      }
-    }
-  }
-
   .password {
     margin-bottom: 20px;
   }
