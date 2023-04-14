837c3f83ade6ef1640e28724c800a03f7eb3b970
Sergio <sergiot.fontal@gmail.com>
2023-04-14 14:13:00 +0200

    Extract inputs and icons from LoginForm to its own component

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,22 +1,20 @@
 import useUser from "../../hooks/useUser/useUser";
 import { useState } from "react";
 import LoginFormStyled from "./LoginFormStyled";
-import { ReactComponent as Mail } from "../../assets/icons/mail.svg";
-import { ReactComponent as HiddenPassword } from "../../assets/icons/hide.svg";
-import { ReactComponent as UnhiddenPassword } from "../../assets/icons/unhide.svg";
 import { ReactComponent as Birdy } from "../../assets/images/birdy.svg";
 import Button from "../Button/Button";
 import LoadingButton from "../LoadingButton/LoadingButton";
-import { useAppDispatch } from "../../store/hooks";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import {
   ableSubmitActionCreator,
   disableSubmitActionCreator,
+  hidePasswordActionCreator,
 } from "../../store/features/uiSlice/uiSlice";
+import Input from "../Input/Input";
 
 const initialFormData = {
   email: "",
   password: "",
-  isHidden: true,
   isInputDisabled: true,
   isLoading: false,
 };
@@ -24,10 +22,10 @@ const initialFormData = {
 const LoginForm = (): JSX.Element => {
   const { loginUser } = useUser();
   const dispatch = useAppDispatch();
+  const { isPasswordHide } = useAppSelector((state) => state.ui);
 
   const [email, setEmail] = useState(initialFormData.email);
   const [password, setPassword] = useState(initialFormData.password);
-  const [isHidden, setIsHidden] = useState(initialFormData.isHidden);
   const [isInputDisabled, setIsInputDisabled] = useState(
     initialFormData.isInputDisabled
   );
@@ -56,10 +54,6 @@ const LoginForm = (): JSX.Element => {
     }
   };
 
-  const togglePasswordVisibilityHandler = () => {
-    setIsHidden(!isHidden);
-  };
-
   const onClickSubmitHandler = () => {
     setIsLoading(true);
   };
@@ -71,78 +65,38 @@ const LoginForm = (): JSX.Element => {
 
     setEmail(initialFormData.email);
     setPassword(initialFormData.password);
-    setIsHidden(initialFormData.isHidden);
     setIsInputDisabled(initialFormData.isInputDisabled);
     setIsLoading(initialFormData.isLoading);
     dispatch(disableSubmitActionCreator());
+    dispatch(hidePasswordActionCreator());
   };
 
   return (
     <LoginFormStyled className="login__form form" onSubmit={onSubmitHandler}>
-      <fieldset className="form__email email">
-        <label htmlFor="email">Email</label>
-        <div className="email-box">
-          <input
-            className="email-box__input"
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Introduce your e-mail"
-            aria-placeholder="Introduce your e-mail"
-            autoComplete="off"
-            onChange={emailInputHandler}
-            value={email}
-            required
-          />
-          <div className="email-box__icon icon">
-            <Mail className="icon__email" />
-          </div>
-        </div>
-      </fieldset>
-      <fieldset className="form__password password">
-        <label htmlFor="password">Password</label>
-        <div className="password-box">
-          <input
-            className="password-box__input"
-            type={isHidden ? "password" : "text"}
-            id="password"
-            name="password"
-            placeholder="Introduce your password"
-            aria-placeholder="Introduce your password"
-            autoComplete="off"
-            onChange={passwordInputHandler}
-            value={password}
-            required
-            disabled={isInputDisabled}
-          />
-          <div className="password-box__icon icon">
-            {isHidden ? (
-              <HiddenPassword
-                className="icon__hidden-password"
-                role="img"
-                title="Unhide password"
-                onClick={togglePasswordVisibilityHandler}
-              />
-            ) : (
-              <UnhiddenPassword
-                className="icon__unhidden-password"
-                role="img"
-                title="Hide password"
-                onClick={togglePasswordVisibilityHandler}
-              />
-            )}
-          </div>
-        </div>
-      </fieldset>
+      <Input
+        name="email"
+        type="email"
+        value={email}
+        onChange={emailInputHandler}
+        required={true}
+      />
+      <Input
+        name="password"
+        type={isPasswordHide ? "password" : "text"}
+        value={password}
+        onChange={passwordInputHandler}
+        disabled={isInputDisabled}
+        required={true}
+      />
       <Button
         className="form__submit"
         text="Login!"
         ariaLabel="click to login"
         onClick={onClickSubmitHandler}
       />
-      {isLoading ? (
+      {isLoading && (
         <LoadingButton className="form__loading" ariaLabel="Loading" />
-      ) : null}
+      )}
       <Birdy className="form__image" role="img" />
     </LoginFormStyled>
   );
